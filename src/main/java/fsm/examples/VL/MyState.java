de652bf2fa72db20de76d36cf9ddd53e0bdbdad7
Matz Heitmüller <matz.heitmueller@haw-hamburg.de>
2023-01-01 15:38:17 +0100

    abstract base class

--- src/main/java/fsm/examples/VL/MyState.java
+++ src/main/java/fsm/examples/VL/MyState.java
@@ -2,7 +2,7 @@ package fsm.examples.VL;
 
 import fsm.base.State;
 
-public class MyState extends State<Event, Integer, Object> {
+public abstract class MyState extends State<Event, Integer, Object> {
 
 	public MyState(MyState other) {
 		this(other, null);
